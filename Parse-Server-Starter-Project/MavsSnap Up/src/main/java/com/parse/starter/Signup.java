399ba2c23faa5fc1c9986ce94dc22ca05c8acfb3
Parth Desai <ppdesai@gmail.com>
2017-04-15 19:35:39 -0500

    Login module with parse's integration and Signup module's layout design

---
+++ Parse-Server-Starter-Project/MavsSnap Up/src/main/java/com/parse/starter/Signup.java
@@ -0,0 +1,22 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void login(View myview){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
